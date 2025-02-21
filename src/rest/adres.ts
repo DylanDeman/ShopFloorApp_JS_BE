61d2f551d95e35693ffc795fed35ac6c25009993
Robin Ledoux <robin.ledoux@student.hogent.be>
2025-02-21 19:40:05 +0100

    Added adress API endpoint and added login endpoint

---
+++ src/rest/adres.ts
@@ -0,0 +1,97 @@
+import Router from '@koa/router';
+import Joi from 'joi';
+import * as adresService from '../service/adres';
+import type { BudgetAppContext, BudgetAppState} from '../types/koa';
+import type { KoaContext, KoaRouter } from '../types/koa';
+import type {
+  CreateAdresRequest,
+  CreateAdresResponse,
+  GetAllAdressesReponse,
+  GetAdresByIdResponse,
+  UpdateAdresRequest,
+  UpdateAdresResponse,
+} from '../types/adres';
+import type { IdParams } from '../types/common';
+import validate from '../core/validation';
+import { requireAuthentication } from '../core/auth';
+
+const getAllAdresses = async (ctx: KoaContext<GetAllAdressesReponse>) => {
+  ctx.body = {
+    items: await adresService.getAll(
+    ),
+  };
+};
+getAllAdresses.validationScheme = null;
+
+const createAdres = async (ctx: KoaContext<CreateAdresResponse, void, CreateAdresRequest>) => {
+  const newAdres = await adresService.create({
+    ...ctx.request.body,
+  });
+  ctx.status = 201;
+  ctx.body = newAdres;
+};
+createAdres.validationScheme = {
+  body: {
+    straat: Joi.string(),
+    huisnummer: Joi.string(),
+    stadsnaam: Joi.string(),
+    postcode: Joi.string(),
+    land: Joi.string(),
+  },
+};
+
+const getAdresById = async (ctx: KoaContext<GetAdresByIdResponse, IdParams>) => {
+  ctx.body = await adresService.getById(
+    ctx.params.id,
+  );
+};
+getAdresById.validationScheme = {
+  params: {
+    id: Joi.number().integer().positive(),
+  },
+};
+
+const updateAdres = async (ctx: KoaContext<UpdateAdresResponse, IdParams, UpdateAdresRequest>) => {
+  const updatedAdres = await adresService.updateById(ctx.params.id, {
+    ...ctx.request.body,
+  });
+  
+  ctx.body = { id: ctx.params.id, ...updatedAdres };
+};
+updateAdres.validationScheme = {
+  params: { id: Joi.number().integer().positive() },
+  body: {
+    straat: Joi.string(),
+    huisnummer: Joi.string(),
+    stadsnaam: Joi.string(),
+    postcode: Joi.string(),
+    land: Joi.string(),
+  },
+};
+
+const deleteAdres = async (ctx: KoaContext<void, IdParams>) => {
+  await adresService.deleteById(ctx.params.id);
+  ctx.status = 204;
+};
+deleteAdres.validationScheme = {
+  params: {
+    id: Joi.number().integer().positive(),
+  },
+};
+
+export default function installAdresRoutes (parent: KoaRouter) {
+  const router = new Router<BudgetAppState, BudgetAppContext>({
+    prefix: '/adres',
+  });
+
+  router.use(requireAuthentication);
+
+  router.get('/', validate(getAllAdresses.validationScheme), getAllAdresses);
+  router.post('/', validate(createAdres.validationScheme), createAdres);
+  router.get('/:id', validate(getAdresById.validationScheme), getAdresById);
+  router.put('/:id', validate(updateAdres.validationScheme), updateAdres);
+  router.delete('/:id', validate(deleteAdres.validationScheme), deleteAdres);
+
+  parent.use(router.routes())
+    .use(router.allowedMethods());
+};
---
+++ src/types/adres.ts
@@ -0,0 +1,27 @@
+import type { Entity, ListResponse } from './common';
+
+export interface Adres extends Entity {
+  straat: string,
+  huisnummer: string,
+  stadsnaam: string,
+  postcode: string,
+  land: string,
+}
+
+export interface AdresCreateInput {
+  straat: string,
+  huisnummer: string,
+  stadsnaam: string,
+  postcode: string,
+  land: string,
+}
+
+export interface AdresUpdateInput extends AdresCreateInput {}
+
+export interface CreateAdresRequest extends AdresCreateInput {}
+export interface UpdateAdresRequest extends AdresUpdateInput {}
+
+export interface GetAllAdressesReponse extends ListResponse<Adres> {}
+export interface GetAdresByIdResponse extends Adres {}
+export interface CreateAdresResponse extends GetAdresByIdResponse {}
+export interface UpdateAdresResponse extends GetAdresByIdResponse {}
