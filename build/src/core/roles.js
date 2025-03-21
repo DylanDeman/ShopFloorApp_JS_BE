8648a6d8ff6eeebc973d0335ad04ba6676d7ad79
Robin Ledoux <robin.ledoux@student.hogent.be>
2025-03-21 10:25:52 +0100

    Changed .gitignore to ignore build directory

--- build/src/core/roles.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    ADMINISTRATOR: 'ADMINISTRATOR',
-    MANAGER: 'MANAGER',
-    VERANTWOORDELIJKE: 'VERANTWOORDELIJKE',
-    TECHNIEKER: 'TECHNIEKER',
-};
