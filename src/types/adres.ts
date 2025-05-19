b61cef69bb461dbbdf214bde6ef0a1e9e14f4a79
Milan Dhondt <milan.dhondt@student.hogent.be>
2025-05-19 15:05:57 +0200

    other fixes

--- src/types/adres.ts
+++ src/types/adres.ts
@@ -5,7 +5,7 @@ export interface Adres extends Entity {
   number: string,
   city: string,
   postalcode: string,
-  land: string,
+  land: string | null,
 }
 
 export interface AdresCreateInput {
@@ -13,15 +13,15 @@ export interface AdresCreateInput {
   number: string,
   city: string,
   postalcode: string,
-  land: string,
+  land: string | null,
 }
 
-export interface AdresUpdateInput extends AdresCreateInput {}
+export interface AdresUpdateInput extends AdresCreateInput { }
 
-export interface CreateAdresRequest extends AdresCreateInput {}
-export interface UpdateAdresRequest extends AdresUpdateInput {}
+export interface CreateAdresRequest extends AdresCreateInput { }
+export interface UpdateAdresRequest extends AdresUpdateInput { }
 
-export interface GetAllAdressesReponse extends ListResponse<Adres> {}
-export interface GetAdresByIdResponse extends Adres {}
-export interface CreateAdresResponse extends GetAdresByIdResponse {}
-export interface UpdateAdresResponse extends GetAdresByIdResponse {}
+export interface GetAllAdressesReponse extends ListResponse<Adres> { }
+export interface GetAdresByIdResponse extends Adres { }
+export interface CreateAdresResponse extends GetAdresByIdResponse { }
+export interface UpdateAdresResponse extends GetAdresByIdResponse { }
